5351bdb05f017ea55f5f019c51c9d5c4390a8d77
Raj Shah <rajshah2903@gmail.com>
2017-02-16 16:27:21 -0800

    Add files via upload

---
+++ oes.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <title>Online Examination System</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" type="text/css" href="oes.css"/>
+	 <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+<!--    <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Raleway:700,600,400,300,200">-->
+    <link type="text/css" rel="stylesheet" href="assets/libs/font-awesome/css/font-awesome.min.css">
+    <link type="text/css" rel="stylesheet" href="assets/libs/ionicons/css/ionicons.min.css">
+    <link type="text/css" rel="stylesheet" href="assets/vendors/medical-icons/style.css">
+    <link type="text/css" rel="stylesheet" href="assets/libs/bootstrap/css/bootstrap.min.css">
+    <link type="text/css" rel="stylesheet" href="assets/libs/animate.css/animate.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/core.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/layout.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/vendor.css">
+    <link type="text/css" rel="stylesheet" href="assets/css/pages/index.css">
+    <!--if lt IE 9-->
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  </head>
+  <body>
+      
+      
+      <div id="container">
+            
+                <div class="header">
+                <img style="margin:10px 2px 2px 10px;float:left;" height="80" width="200" src="images/logo.gif" alt="OES"/><h3 class="headtext"> &nbsp;Online Examination System </h3><h4 style="color:#ffffff;text-align:center;margin:0 0 5px 5px;"><i>...because Examination Matters</i></h4>
+            </div>
+     <form id="stdloginform" action="index.php" method="post">
+      <div class="menubar">
+       </br>
+	   </br>
+	   </br>
+         <div class="section-content">
+                        <div class="list-departments">
+                            <div class="row mbxxl" align="center">
+                                <div class="col-md-4 col-sm-3 col-xs-3"><a href="http://localhost:81/OnlineExamination1/index.php"><img src="http://localhost:81/OnlineExamination1//assets/images/MetaIcon-STResearchIcon.png" alt="" class="img-responsive"/>
+
+                                    <p>Student Login</p></a></div>
+                                <div class="col-md-4 col-sm-3 col-xs-3"><a href="http://localhost:81/OnlineExamination1/tc/index.php"><img src="http://localhost:81/OnlineExamination1//assets/images/webicons_ed2.png" alt="" class="img-responsive"/>
+
+                                    <p>Faculty Login</p></a></div>
+                                <div class="col-md-4 col-sm-3 col-xs-3"><a href="http://localhost:81/OnlineExamination1/admin/index.php"><img src="http://localhost:81/OnlineExamination1//assets/images/Admin.gif" alt="" class="img-responsive"/>
+
+                                    <p>Admin Login</p></a></div>
+									
+                                
+                            </div>
+                           
+                        </div>
+
+      </div>
+      <div class="page">
+              
+             
+
+
+      </div>
+       </form>
+
+    <div id="footer">
+          <p style="font-size:70%;color:#ffffff;"> Developed By-<b>Team-1</b><br/> </p><p>cs595(M)Capstone project</p>
+      </div>
+      </div>
+	  <script src="assets/js/jquery-1.11.2.min.js"></script>
+<script src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+<script src="assets/libs/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/libs/bootstrap-hover-dropdown/bootstrap-hover-dropdown.js"></script>
+<script src="assets/js/html5shiv.js"></script>
+<script src="assets/js/respond.min.js"></script>
+<script src="assets/js/layout.js"></script>
+<!--LOADING SCRIPTS FOR PAGE-->
+<script src="assets/js/jquery.appear.js"></script>
+<script src="assets/js/pages/index.js"></script>
+<!--CORE JAVASCRIPT-->
+<script src="assets/js/main.js"></script>
+<script src="assets/js/layout.js"></script>
+  </body>
+</html>
